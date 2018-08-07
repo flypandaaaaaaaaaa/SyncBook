55d3fb5501d508f139193f4a9dd5acead2adef9c
fly panda <fllypanda@outlook.com>
2018-08-07 22:03:34 +0800

    2

--- app.py
+++ app.py
@@ -6,8 +6,20 @@ app.config['SECRET_KEY']='XXXXXX'
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    command_result=os.popen('ipconfig')
-    filesystem=command_result.readlines()
+    # command_result=os.popen('ipconfig')
+    # filesystem=command_result.readlines()
+    filesystem=[]
+    with open('D:\\filesystem.txt','r') as f:
+        fstxt=f.readlines()
+    for i in fstxt:
+        single_fs=i.split()
+        if len(i.split()[0].split('/'))==4 and 'mapper' in i.split()[0].split('/'):
+            single_fs.append(i.split()[0].split('/')[3])
+        else:
+            single_fs.append('None')
+        filesystem.append(single_fs)
+        print(single_fs)
+    filesystem.pop(0)
     return render_template('index.html',filesystem=filesystem)
 
 if __name__ == '__main__':
